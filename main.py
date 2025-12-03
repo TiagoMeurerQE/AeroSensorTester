838015875d06208bf1c5306ba215d5bbf9f1ff7c
TiagoMeurerQE <quantumenginelab@gmail.com>
2025-12-03 20:28:03 -0300

    ✨ Implemented model building/training

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import ctypes
-
+import numpy as np
+import tensorflow as tf
 
 lib = ctypes.CDLL('./sensor_sim.so')
 lib.simulate_sensor_data.restype = ctypes.POINTER(ctypes.c_float)
 lib.simulate_sensor_data.argtypes = [ctypes.c_int, ctypes.c_float, ctypes.c_float, ctypes.c_int]
+
+# Model to building/training the simple anomaly (w/ unsupervised detection)
+def build_anomaly_model(input_dim):
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(32, activation='relu', input_shape=(input_dim,)),
+        tf.keras.layers.Dense(16, activation='relu'),
+        tf.keras.layers.Dense(32, activation='relu'),
+        tf.keras.layers.Dense(input_dim, activation='sigmoid')
+    ])
+    model.compile(optimizer='adam', loss='mse')
+    return model
+
